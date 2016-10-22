7eedef7ad69181bcb4eb9c10efd24f19bc00b3c0
AV-ZhangJ <av-zhangj@AV-ZhangJ-MBP.fios-router.home>
2016-10-21 21:03:11 -0400

    New UI

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+
+(function() {
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.inner-section',{ duration: 1000 });
+console.log('h');
+})();
+
