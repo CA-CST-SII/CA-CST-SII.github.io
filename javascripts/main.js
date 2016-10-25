8f04d2625eadcb99852e1ad70246f30a4691a231
AV-ZhangJ <av-zhangj@AV-ZhangJ-MBP.fios-router.home>
2016-10-25 18:41:38 -0400

    Changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,26 @@
 
-(function() {
-window.sr = ScrollReveal({reset: true});
-sr.reveal('.inner-section',{ duration: 1000 });
-console.log('h');
-})();
+$(function() {
+window.sr = ScrollReveal({reset: true},{scale: 0.1});
+sr.reveal('.link-anim',{ duration: 500 });
+sr.reveal('#header_wrap .inner',{ duration: 1000 });
+sr.reveal('#section-one .inner-section',{ duration: 1300 });
+sr.reveal('#section-two .inner-section p',{ duration: 1300 });
+sr.reveal('#section-three .inner-section p',{ duration: 1300 });
+sr.reveal('#section-four .inner-section',{ duration: 1300 });
 
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
